801e2c69c67eb8f574128364f3116338e56c397a
lukeify <5379845+lukeify@users.noreply.github.com>
2025-01-12 15:59:41 +1300

    feat: mkdir -p /usr/local/bin

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+
+if [[ ! -d /usr/local/bin ]]; then
+  echo "Creating /usr/local/bin directory"
+  mkdir -p /usr/local/bin
+fi
+
 for file in src/*.sh; do
-  cp "$file" "${file%.sh}"
+  filename=$(basename "$file" .sh)
+  cp "$file" "/usr/local/bin/$filename"
 done
